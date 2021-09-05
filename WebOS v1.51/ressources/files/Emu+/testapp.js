87d593470d083f3e440ad2a54117aa67454df581
yannou <yannnonoficiel@gmail.com>
2021-09-05 16:04:34 +0200

    importation of code

---
+++ WebOS v1.51/ressources/files/Emu+/testapp.js
@@ -0,0 +1,1 @@
+document.getElementById('screen').innerHTML='test';
---
+++ WebOS v1.51/task/bundle.js
@@ -0,0 +1,50 @@
+const { base64Sync } = require('base64-img');
+const { writeFileSync, readFileSync } = require('fs');
+
+const image = process.argv[2] === "--image";
+//const template = process.argv[2] === "--template";
+const style = process.argv[2] === "--style";
+
+(function(){
+
+    if(image){
+
+        // TODO provide custom filenames
+
+        const compressed = {
+
+            max: base64Sync('dist/img/max.svg'),
+            close: base64Sync('dist/img/close.svg'),
+            full: base64Sync('dist/img/full.svg'),
+            //exit: base64Sync('dist/img/exit.svg'),
+            min: base64Sync('dist/img/min.svg')
+        };
+
+        let tmp = "";
+
+        for(let key in compressed){
+
+            if(compressed.hasOwnProperty(key)){
+
+                tmp += ("@" + key + ": \"" + compressed[key] + "\";\n");
+            }
+        }
+
+        writeFileSync("tmp/images.less", tmp);
+        writeFileSync("tmp/bundle.less", '@import "../src/css/winbox.less"; @import "images.less";');
+    }
+
+    // ----------------------
+
+    if(style){
+
+        writeFileSync("tmp/style.js",
+
+            'const style = document.createElement("style");' +
+            'style.innerHTML = "' + readFileSync("dist/css/winbox.min.css", "utf8").replace(/"/g, "'") + '";' +
+            'const head = document.getElementsByTagName("head")[0];' +
+            'if(head.firstChild) head.insertBefore(style, head.firstChild); else head.appendChild(style);'
+        );
+    }
+
+})();
